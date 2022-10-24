b4bf536d3c45fa88b951dc70f174a9e92792e279
ohy1023 <zvyg1023@naver.com>
2022-10-24 10:09:29 +0900

    KthNum 배열 / 우선순위 큐

--- src/main/java/algorithm/java1024/KthNum.java
+++ src/main/java/algorithm/java1024/KthNum.java
@@ -1,9 +1,10 @@
 package algorithm.java1024;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class KthNum {
-    public int[] solution(int[] arr, int[][] commands) {
+    public int[] solutionByArray(int[] arr, int[][] commands) {
         int[] answer = new int[commands.length];
         int idx = 0;
         for (int[] command : commands) {
@@ -14,5 +15,30 @@ public class KthNum {
         return answer;
     }
 
+    private int[] arr;
+
+    public int getKthNum(int[] command) {
+        int frIdx = command[0];
+        int toIdx = command[1];
+        int nth = command[2];
+        int result = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = frIdx - 1; i < toIdx; i++) {
+            pq.add(arr[i]);
+        }
+        for (int i = 0; i < nth; i++) {
+            result = pq.poll();
+        }
+        return result;
+    }
+
+    public int[] solutionByPriorityQueue(int[] arr, int[][] commands) {
+        this.arr = arr;
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = getKthNum(commands[i]);
+        }
+        return answer;
+    }
 
 }
