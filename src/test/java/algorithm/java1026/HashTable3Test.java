f0fa0b47b623a2bcd3d8296960f0b12de5a0d13a
ohy1023 <zvyg1023@naver.com>
2022-10-26 10:39:37 +0900

    해쉬 충돌 테스트

---
+++ src/test/java/algorithm/java1026/HashTable3Test.java
@@ -0,0 +1,21 @@
+package algorithm.java1026;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HashTable3Test {
+    @Test
+    @DisplayName("해쉬 추돌 문제 테스트")
+    void insertAndSearchTest() {
+
+        HashTable3 ht = new HashTable3();
+        ht.insert("Yoonseo", 1);
+        ht.insert("Seoyoon", 2);
+
+        Assertions.assertEquals(1, ht.search("Yoonseo"));
+        Assertions.assertEquals(2, ht.search("Seoyoon"));
+
+    }
+
+}
