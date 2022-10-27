5560d0a0bd9a9ffe00b39251b0bc9495144503a7
ohy1023 <zvyg1023@naver.com>
2022-10-27 09:37:18 +0900

    도전중

--- src/main/java/algorithm/java1027/HashPlayer.java
+++ src/main/java/algorithm/java1027/HashPlayer.java
@@ -8,28 +8,41 @@ public class HashPlayer {
         private Integer key;
         private String value;
 
+        public Integer getKey() {
+            return key;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
         public Player(Integer key, String value) {
             this.key = key;
             this.value = value;
+
+
         }
     }
     public String solution(String[] participant, String[] completion) {
         int sum = 0;
         String answer = "";
-        List<Player>[] lists = new ArrayList[100];
-        for (String s : participant) {
-            lists[s.hashCode()].add(new Player(s.hashCode(),s));
-            sum += s.hashCode();
+        List<Player>[] lists = new ArrayList[10];
+        for (String p : participant) {
+            if (lists[0] == null) {
+                lists[0] = new ArrayList<>();
+            }
+            lists[0].add(new Player(p.hashCode(),p));
+            sum += p.hashCode();
         }
-        for (String s : completion) {
-
+        for (String c : completion) {
+            sum -= c.hashCode();
         }
+        System.out.println(sum);
         return answer;
     }
 
     public static void main(String[] args) {
         HashPlayer hp = new HashPlayer();
-
         String[] participant = new String[]{"mislav", "stanko", "mislav", "ana"};
         String[] completion = new String[]{"stanko", "ana", "mislav"};
         System.out.println(hp.solution(participant,completion));
