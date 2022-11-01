d246d8b781bf76a67ea2a5da910b272f1d501947
ohy1023 <zvyg1023@naver.com>
2022-11-01 10:19:14 +0900

    탬플릿 콜백 1단계 : 하나만 분리하기

--- src/main/java/algorithm/java1101/Prime.java
+++ src/main/java/algorithm/java1101/Prime.java
@@ -1,8 +1,13 @@
 package algorithm.java1101;
 
 public class Prime {
-    boolean isPrime(int num) {
-        for (int j = 2; j < num; j++) {
+
+    boolean someOperation(int a, int b) {
+        return a < b;
+    }
+
+    boolean isPrime(int num ) {
+        for (int j = 2; someOperation(j,num); j++) {
             if (num % j == 0) {
                 return false;
             }
