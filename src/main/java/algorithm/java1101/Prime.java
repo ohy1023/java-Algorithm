8baecee0935f1233fa9cee169c1405b56b6e2150
ohy1023 <zvyg1023@naver.com>
2022-11-01 10:28:42 +0900

    탬플릿 콜백 2단계 : 인터페이스

--- src/main/java/algorithm/java1101/Prime.java
+++ src/main/java/algorithm/java1101/Prime.java
@@ -1,13 +1,17 @@
 package algorithm.java1101;
 
+
+interface StatementStrategy {
+    boolean compare(int a, int b);
+}
 public class Prime {
 
     boolean someOperation(int a, int b) {
         return a < b;
     }
 
-    boolean isPrime(int num ) {
-        for (int j = 2; someOperation(j,num); j++) {
+    boolean isPrime(int num, StatementStrategy stmt) {
+        for (int j = 2; stmt.compare(j,num); j++) {
             if (num % j == 0) {
                 return false;
             }
