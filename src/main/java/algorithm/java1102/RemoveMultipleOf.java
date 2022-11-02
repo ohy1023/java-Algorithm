8d17f480f086cc0b5ea799ce916c8818cdf14323
ohy1023 <zvyg1023@naver.com>
2022-11-02 09:45:54 +0900

    에라토스테네스의 체 1단계 : 2의 배수 지우기

--- src/main/java/algorithm/java1102/RemoveMultipleOf.java
+++ src/main/java/algorithm/java1102/RemoveMultipleOf.java
@@ -10,10 +10,14 @@ public class RemoveMultipleOf {
         for (int i = 2; i < cnt; i++) {
             arrayList.add(i);
         }
-        for (int j = 0; j < arrayList.size()-2; j++) {
-            arrayList.remove(j+2);
+        for (int j = 0; j < arrayList.size(); j++) {
+            if (arrayList.get(j) % 2 == 0 && arrayList.get(j) > 2) {
+                arrayList.remove(j);
+            }
         }
 
+
+//        arrayList.removeIf(num -> num % 2 == 0 && num != 2);
 
         System.out.println(arrayList);
         System.out.println(arrayList.size());
