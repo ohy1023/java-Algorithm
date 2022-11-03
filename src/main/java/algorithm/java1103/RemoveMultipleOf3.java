5830d790181a1a0b67a67b66c016bc3197513e4f
ohy1023 <zvyg1023@naver.com>
2022-11-03 10:39:54 +0900

    에라토스테네스의 체

--- src/main/java/algorithm/java1103/RemoveMultipleOf3.java
+++ src/main/java/algorithm/java1103/RemoveMultipleOf3.java
@@ -10,22 +10,20 @@ public class RemoveMultipleOf3 {
         boolean[] checks = new boolean[arr.length];
         Arrays.fill(checks, true);
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i * i < arr.length; i++) {
             arr[i] = i + 2;
         }
         for (int i = 2; i < arr.length; i++) {
-            for (int j = 2*(i-1); j < arr.length; j += i) {
+            for (int j = 2 * (i - 1); j < arr.length; j += i) {
                 checks[j] = false;
             }
         }
 
         for (int i = 0; i < checks.length; i++) {
             if (checks[i]) {
-                System.out.print(arr[i] + " ");
                 cnt++;
             }
         }
-        System.out.println();
         System.out.println(cnt);
     }
 }
