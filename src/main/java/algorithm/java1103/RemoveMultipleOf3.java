29482066d513e691d9e4282be65a717e45a19b1d
ohy1023 <zvyg1023@naver.com>
2022-11-03 10:12:34 +0900

    에라토스테네스의 체 - 2의 배수 지우고 출력

--- src/main/java/algorithm/java1103/RemoveMultipleOf3.java
+++ src/main/java/algorithm/java1103/RemoveMultipleOf3.java
@@ -1,24 +1,26 @@
 package algorithm.java1103;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class RemoveMultipleOf3 {
     public static void main(String[] args) {
         int N = 50;
-        int[] arr = new int[N-1];
+        int[] arr = new int[N - 1];
         boolean[] checks = new boolean[arr.length];
-        Arrays.fill(checks,true);
+        Arrays.fill(checks, true);
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = i +2;
-        }
-        System.out.println(Arrays.toString(arr));
-
-        for (int i = 0; i < arr.length; i++) {
-
-
+            arr[i] = i + 2;
         }
 
+        for (int j = 2; j < arr.length; j += 2) {
+            checks[j] = false;
+        }
+
+        for (int i = 0; i < checks.length; i++) {
+            if (checks[i]) {
+                System.out.print(arr[i] + " ");
+            }
+        }
     }
 }
