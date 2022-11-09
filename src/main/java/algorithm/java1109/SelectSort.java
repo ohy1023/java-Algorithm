422c56c9d4ef13e5553627726e03e1fd8190faa5
ohy1023 <zvyg1023@naver.com>
2022-11-09 09:36:20 +0900

    [선택정렬] i가 0,1일때 최소 idx 찾아서 swap

--- src/main/java/algorithm/java1109/SelectSort.java
+++ src/main/java/algorithm/java1109/SelectSort.java
@@ -1,28 +1,42 @@
 package algorithm.java1109;
 
 
+import java.util.Arrays;
+
 public class SelectSort {
 
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
-        int minIdx = 0;
 
         // i가 0일때 최소값 idx 찾기
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[0] > arr[i]) {
-                minIdx = i;
+        int minIdx = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[minIdx] > arr[j]) {
+                minIdx = j;
             }
         }
+        int temp = arr[0];
+        arr[0] = arr[minIdx];
+        arr[minIdx] = temp;
+
         System.out.println(minIdx);
+        System.out.println(Arrays.toString(arr));
 
         // i가 1일때 최소값 idx 찾기
-        for (int i = 2; i < arr.length; i++) {
-            if (arr[1] > arr[i]) {
-                minIdx = i;
+        minIdx = 1;
+        for (int j = 2; j < arr.length; j++) {
+            if (arr[minIdx] > arr[j]) {
+                minIdx = j;
             }
         }
+        temp = arr[1];
+        arr[1] = arr[minIdx];
+        arr[minIdx] = temp;
+
         System.out.println(minIdx);
+        System.out.println(Arrays.toString(arr));
 
 
     }
+
 }
