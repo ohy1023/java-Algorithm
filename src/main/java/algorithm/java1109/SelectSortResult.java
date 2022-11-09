c3e7c23fd51d1e18ce08a857a31a734f79ee9903
ohy1023 <zvyg1023@naver.com>
2022-11-09 09:40:02 +0900

    [선택정렬] 최종

--- src/main/java/algorithm/java1109/SelectSortResult.java
+++ src/main/java/algorithm/java1109/SelectSortResult.java
@@ -2,18 +2,17 @@ package algorithm.java1109;
 
 import java.util.Arrays;
 
-public class SelectSort2 {
+public class SelectSortResult {
     int[] solution(int[] arr) {
-        for (int i = 0; i < arr.length -1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             int idx = i;
-            for (int j = i+1; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[idx] > arr[j]) {
                     idx = j;
                 }
             }
             swap(arr, i, idx);
         }
-
 
         return arr;
     }
@@ -26,7 +25,7 @@ public class SelectSort2 {
 
     public static void main(String[] args) {
         int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
-        SelectSort2 ss = new SelectSort2();
+        SelectSortResult ss = new SelectSortResult();
         System.out.println(Arrays.toString(ss.solution(arr)));
 
 
