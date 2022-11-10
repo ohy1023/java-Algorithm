1038ccd3ca3467c0e50e2af245e3ce759d0805d6
ohy1023 <zvyg1023@naver.com>
2022-11-10 09:41:16 +0900

    [프로그래머스 같은숫자는싫어] 스택 추가

--- src/main/java/algorithm/java1110/ArraysNum.java
+++ src/main/java/algorithm/java1110/ArraysNum.java
@@ -3,9 +3,10 @@ package algorithm.java1110;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 public class ArraysNum {
-    int[] solution(int[] arr) {
+    int[] solutionByList(int[] arr) {
         List<Integer> integerList = new ArrayList<>();
         integerList.add(arr[0]);
         for (int i = 1; i < arr.length; i++) {
@@ -22,9 +23,26 @@ public class ArraysNum {
         return answer;
     }
 
+    int[] solutionByStack(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (stack.peek() != arr[i]) {
+                stack.push(arr[i]);
+            }
+        }
+        int[] answer = new int[stack.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = stack.get(i);
+        }
+        return answer;
+    }
+
+
     public static void main(String[] args) {
         ArraysNum an = new ArraysNum();
         int[] arr = new int[]{1, 1, 3, 3, 0, 1, 1};
-        System.out.println(Arrays.toString(an.solution(arr)));
+        System.out.println(Arrays.toString(an.solutionByList(arr)));
+        System.out.println(Arrays.toString(an.solutionByStack(arr)));
     }
 }
