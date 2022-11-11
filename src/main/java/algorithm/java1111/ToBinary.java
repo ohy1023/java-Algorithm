370eedd53f2cac108645217d7e934256785d1319
ohy1023 <zvyg1023@naver.com>
2022-11-11 09:35:08 +0900

    10 -> 2 진수 하는중

--- src/main/java/algorithm/java1111/ToBinary.java
+++ src/main/java/algorithm/java1111/ToBinary.java
@@ -2,12 +2,18 @@ package algorithm.java1111;
 
 public class ToBinary {
     public static void main(String[] args) {
-        int num = 12;
+        int num = 0;
+        String ans = "";
         while (num > 0) {
             String res = String.valueOf(num % 2);
             num /= 2;
-
+            ans += res;
         }
+        String reverse = "";
+        for (int i = ans.length() - 1; i >= 0; i--) {
+            reverse += ans.charAt(i);
+        }
+        System.out.println(reverse);
 
     }
 }
