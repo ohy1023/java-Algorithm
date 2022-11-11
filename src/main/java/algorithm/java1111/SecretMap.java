ff03bd2ef56fbf169f1cdd1d29b08189c77ef795
ohy1023 <zvyg1023@naver.com>
2022-11-11 10:27:53 +0900

    [프로그래머스 비밀지도] 테스트2 실패

--- src/main/java/algorithm/java1111/SecretMap.java
+++ src/main/java/algorithm/java1111/SecretMap.java
@@ -22,7 +22,7 @@ public class SecretMap {
 
         for (int i = 0; i < n; i++) {
             String res = "";
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < binaryList[i].length(); j++) {
                 if (binaryList[i].charAt(j) == '1') {
                     res += "#";
                 } else {
@@ -69,7 +69,7 @@ public class SecretMap {
 
     public static void main(String[] args) {
         SecretMap sm = new SecretMap();
-        String[] solution = sm.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28});
+        String[] solution = sm.solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10});
         System.out.println(Arrays.toString(solution));
 
     }
