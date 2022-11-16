488159250fd7648a254f742b5690336ba86b98db
ohy1023 <zvyg1023@naver.com>
2022-11-16 09:28:01 +0900

    [퀵 정렬] 1. pivot 값 정하기

---
+++ src/main/java/algorithm/java1116/QuickSortByArray.java
@@ -0,0 +1,30 @@
+package algorithm.java1116;
+
+
+import java.util.Arrays;
+
+/**
+ * 1. pivot을 정한다
+ * 2. pivot보다 arr[leftIdx]가 작으면 leftIdx +=1
+ * 3. pivot보다 arr[rightIdx]가 크면 rightIdx -=1
+ * 4. leftIdx <= rightIdx 면 교환
+ * 5. 교환 했으면 leftIdx+=1, rightIdx -= 1
+ */
+
+public class QuickSortByArray {
+    int[] solution(int[] arr) {
+        int lt = 0;
+        int rt = arr.length - 1;
+        int pivot = arr[(lt+rt)/2];
+        System.out.println(pivot);
+//        while (arr[l]) {
+//        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        QuickSortByArray qsba = new QuickSortByArray();
+        int[] arr = new int[]{20, 18, 5, 19, 40, 50, 5, 25};
+        System.out.println(Arrays.toString(qsba.solution(arr)));
+    }
+}
