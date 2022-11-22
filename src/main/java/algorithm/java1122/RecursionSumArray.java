9d358ae0ce6d8ecbc2036f1bebd781d302b074dd
ohy1023 <zvyg1023@naver.com>
2022-11-22 09:43:25 +0900

    [likelion] 재귀로 배열의 합 구하기

---
+++ src/main/java/algorithm/java1122/RecursionSumArray.java
@@ -0,0 +1,17 @@
+package algorithm.java1122;
+
+public class RecursionSumArray {
+    public static int sum(int[] arr, int n, int res) {
+        if (n < 0) {
+            return res;
+        }
+        res += arr[n];
+        return sum(arr, n - 1, res);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{7, 3, 2, 9};
+        System.out.println(sum(arr, arr.length - 1, 0));
+
+    }
+}
