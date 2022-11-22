31fa35f127caf5068503ec7ef90c4c14205c1be6
ohy1023 <zvyg1023@naver.com>
2022-11-22 09:20:53 +0900

    [likelion] 재귀로 1부터 100까지 출력

--- src/main/java/algorithm/java1122/Recursion.java
+++ src/main/java/algorithm/java1122/Recursion.java
@@ -2,9 +2,11 @@ package algorithm.java1122;
 
 public class Recursion {
     public static void printTo100(int n) {
-
+        if (n > 100) {
+            return;
+        }
         System.out.println(n);
-        printTo100(n);
+        printTo100(n+1);
     }
 
     public static void main(String[] args) {
