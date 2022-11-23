3a8fbbe6da15913830267e07a8b450f80030f67d
ohy1023 <zvyg1023@naver.com>
2022-11-23 09:24:26 +0900

    [CodeUp] 피보나치 수열 - 재귀

---
+++ src/main/java/algorithm/java1123/FibonacciSequence.java
@@ -0,0 +1,21 @@
+package algorithm.java1123;
+
+import java.util.Scanner;
+
+public class FibonacciSequence {
+    public int solution(int num) {
+        if (num == 0) {
+            return 0;
+        } else if (num == 1) {
+            return 1;
+        } else {
+            return solution(num - 1) + solution(num - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        FibonacciSequence fs = new FibonacciSequence();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(fs.solution(sc.nextInt()));
+    }
+}
