5e38f59b71ff6677b885f3dc00a25fd2a4535a69
ohy1023 <zvyg1023@naver.com>
2022-11-23 09:16:53 +0900

    [CodeUp] 팩토리얼 계산 - 재귀

---
+++ src/main/java/algorithm/java1123/FactorialCalculation.java
@@ -0,0 +1,18 @@
+package algorithm.java1123;
+
+import java.util.Scanner;
+
+public class FactorialCalculation {
+    public int solution(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        return num * solution(num - 1);
+    }
+
+    public static void main(String[] args) {
+        FactorialCalculation fc = new FactorialCalculation();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(fc.solution(sc.nextInt()));
+    }
+}
