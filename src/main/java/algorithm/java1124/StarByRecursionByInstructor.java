94827b8064924b898463c1425de50439b9544b53
ohy1023 <zvyg1023@naver.com>
2022-11-24 10:25:51 +0900

    [CodeUp] 재귀로 별 찍기

---
+++ src/main/java/algorithm/java1124/StarByRecursionByInstructor.java
@@ -0,0 +1,30 @@
+package algorithm.java1124;
+
+import java.util.Scanner;
+
+public class StarByRecursionByInstructor {
+
+    public void printStar(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.print("*");
+        printStar(n-1);
+    }
+
+    public void printRow(int n) {
+        if (n == 0) {
+            return;
+        }
+        printRow(n - 1);
+        printStar(n);
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StarByRecursionByInstructor star = new StarByRecursionByInstructor();
+        star.printRow(sc.nextInt());
+    }
+}
