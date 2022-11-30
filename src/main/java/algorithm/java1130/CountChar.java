a5ecfd5b9ac0fa7d51604d569d397a304435a01b
ohy1023 <zvyg1023@naver.com>
2022-11-30 10:39:03 +0900

    [Programmers] 문자열 내 p와 y의 개수

---
+++ src/main/java/algorithm/java1130/CountChar.java
@@ -0,0 +1,34 @@
+package algorithm.java1130;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.Locale;
+
+public class CountChar {
+    boolean solution(String s) {
+        int cntP = 0;
+        int cntY = 0;
+        String upperCase = s.toUpperCase();
+        for (int i = 0; i < upperCase.length(); i++) {
+            if (upperCase.charAt(i) == 'P') {
+                cntP++;
+            } else if (upperCase.charAt(i) == 'Y') {
+                cntY++;
+            }
+        }
+        if (cntP == cntY) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        CountChar cc = new CountChar();
+        System.out.println(cc.solution(s));
+    }
+}
