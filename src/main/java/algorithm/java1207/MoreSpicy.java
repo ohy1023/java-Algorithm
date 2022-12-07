de7b7ffcef639976eaeb32a3a9dbee8c8bb58f18
ohy1023 <zvyg1023@naver.com>
2022-12-07 10:23:44 +0900

    [Programmers] 더 맵게 - 우선순위 큐

---
+++ src/main/java/algorithm/java1207/MoreSpicy.java
@@ -0,0 +1,28 @@
+package algorithm.java1207;
+
+import java.util.PriorityQueue;
+
+public class MoreSpicy {
+    public int solution(int[] scoville, int K) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < scoville.length; i++) {
+            pq.add(scoville[i]);
+        }
+
+        int answer = 0;
+        while (pq.peek() < K) {
+            if (pq.size() == 1) {
+                return -1
+            }
+            int mix = pq.poll() + (pq.poll() * 2);
+            pq.add(mix);
+            answer++;
+        }
+
+        return answer;
+    }
+
+
+}
