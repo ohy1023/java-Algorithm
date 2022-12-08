ae37c4148ed000cb23ee03a24a7a5672cc3bd618
ohy1023 <zvyg1023@naver.com>
2022-12-08 09:50:15 +0900

    [LikeLion] 피보나치 수열 - DP

--- src/main/java/algorithm/java1208/FibonacciByDP.java
+++ src/main/java/algorithm/java1208/FibonacciByDP.java
@@ -9,7 +9,6 @@ public class FibonacciByDP {
         }
         for (int i = 2; i <= n; i++) {
             memo[i] = memo[i - 1] + memo[i - 2];
-            System.out.println("dfds");
         }
         return memo[n];
     }
