2727481f448a80159d370a0681967ae2144e8622
ohy1023 <zvyg1023@naver.com>
2022-12-08 09:49:49 +0900

    [LikeLion] 피보나치 수열 - DP

--- src/main/java/algorithm/java1208/FibonacciByDP.java
+++ src/main/java/algorithm/java1208/FibonacciByDP.java
@@ -4,15 +4,18 @@ public class FibonacciByDP {
     private long findFibonacci(int n) {
         long[] memo = new long[n + 1];
         memo[0] = 1;
-        memo[1] = 1;
+        if (n > 0) {
+            memo[1] = 1;
+        }
         for (int i = 2; i <= n; i++) {
             memo[i] = memo[i - 1] + memo[i - 2];
+            System.out.println("dfds");
         }
         return memo[n];
     }
 
     public static void main(String[] args) {
         FibonacciByDP dp = new FibonacciByDP();
-        System.out.println(dp.findFibonacci(5));
+        System.out.println(dp.findFibonacci(1));
     }
 }
