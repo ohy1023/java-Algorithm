6cfd9d006aecb4d91b7116d79f1d053e766c446b
ohy1023 <zvyg1023@naver.com>
2022-12-09 10:42:29 +0900

    [CodeUp] 사투리

--- src/main/java/algorithm/java1209/LCS.java
+++ src/main/java/algorithm/java1209/LCS.java
@@ -1,12 +1,13 @@
 package algorithm.java1209;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class LCS {
-    public static void main(String[] args) {
-        String str1 = "ABCDCBA"; // -> j로 놓고 안쪽에서 반복 ( 열 )
-        String str2 = "DCABDC"; // -> i로 놓고 바깥쪽에서 반복 ( 행 )
 
-
+    private int solution(String str1, String str2) {
         int[][] dp = new int[str2.length() + 1][str1.length() + 1];
 
         for (int i = 1; i <= str2.length(); i++) {
@@ -14,19 +15,20 @@ public class LCS {
                 if (str2.charAt(i - 1) == str1.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    if (dp[i - 1][j] > dp[i][j - 1]) {
-                        dp[i][j] = dp[i - 1][j];
-                    } else {
-                        dp[i][j] = dp[i][j - 1];
-                    }
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                System.out.print(dp[i][j] + " ");
-            }
-            System.out.println();
-        }
+        return dp[str2.length()][str1.length()];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+
+        LCS lcs = new LCS();
+        System.out.println(lcs.solution(str1, str2));
+
+
     }
 }
