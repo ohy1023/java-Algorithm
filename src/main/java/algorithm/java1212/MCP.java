24e276ab1251dd73fe446f52fdcf7d3517077947
ohy1023 <zvyg1023@naver.com>
2022-12-12 09:42:19 +0900

    [LikeLion] 최소비용경로 - 첫쨰열, 첫째행만 있는 경우

--- src/main/java/algorithm/java1212/MCP.java
+++ src/main/java/algorithm/java1212/MCP.java
@@ -1,12 +1,18 @@
 package algorithm.java1212;
 
+import java.util.Arrays;
+
 public class MCP {
     private int solution(int[][] arr) {
         int[][] dp = new int[arr.length][arr[0].length];
 
         for (int i = 1; i < arr.length; i++) {
             for (int j = 1; j < arr[0].length; j++) {
-                dp[i][j] = dp[i][j - 1] + arr[i][j];
+                if (i == 1) {
+                    dp[i][j] = dp[i][j - 1] + arr[i][j];
+                } else if (j == 1) {
+                    dp[i][j] = dp[i - 1][j] + arr[i][j];
+                }
                 System.out.print(dp[i][j] + " ");
             }
             System.out.println();
@@ -16,7 +22,7 @@ public class MCP {
 
     public static void main(String[] args) {
         MCP mcp = new MCP();
-        int[][] arr = new int[][]{{0, 0, 0, 0},{0, 1, 3, 2}, {0, 4, 6, 2}, {0, 1, 2, 4}};
+        int[][] arr = new int[][]{{0, 0, 0, 0}, {0, 1, 3, 2}, {0, 4, 6, 2}, {0, 1, 2, 4}};
 
         System.out.println((mcp.solution(arr)));
     }
