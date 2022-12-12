6b46934caab66ab192c6166cb57e540102e0ca81
ohy1023 <zvyg1023@naver.com>
2022-12-12 09:32:52 +0900

    [LikeLion] 최소비용경로 - 3 x 3 dp 배열 만들기

---
+++ src/main/java/algorithm/java1212/MCP.java
@@ -0,0 +1,24 @@
+package algorithm.java1212;
+
+public class MCP {
+    private int solution(int[][] arr) {
+        int[][] dp = new int[arr.length][arr[0].length];
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j < arr[0].length; j++) {
+                dp[i][j] = dp[i][j - 1] + arr[i][j];
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        MCP mcp = new MCP();
+        int[][] arr = new int[][]{{0, 0, 0, 0},{0, 1, 3, 2}, {0, 4, 6, 2}, {0, 1, 2, 4}};
+
+        System.out.println((mcp.solution(arr)));
+    }
+
+}
