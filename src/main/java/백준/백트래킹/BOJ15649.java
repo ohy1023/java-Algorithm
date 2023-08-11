b71a0c6678a9f053215e1fc9aac96d86e69d98d4
ohy1023 <zvyg1023@naver.com>
2023-08-11 18:55:55 +0900

    [백준] N과M (1) - 백트래킹

---
+++ src/main/java/백준/백트래킹/BOJ15649.java
@@ -0,0 +1,51 @@
+package 백준.백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15649 {
+
+    public static int n, m;
+    public static int[] res;
+    public static boolean[] check;
+
+    public static StringBuilder sb = new StringBuilder();
+
+    private static void backTracking(int v) {
+        if (v == m) {
+            for (int val : res) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int i = 1; i <= n; i++) {
+                if (!check[i]) {
+                    check[i] = true;
+                    res[v] = i;
+                    backTracking(v + 1);
+                    check[i] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        res = new int[m];
+        check = new boolean[n + 1];
+
+        backTracking(0);
+        System.out.println(sb);
+
+    }
+
+
+}
