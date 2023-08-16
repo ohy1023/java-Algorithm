615604df20f12fb5e1f04a469cbac27d1671919d
ohy1023 <zvyg1023@naver.com>
2023-08-16 12:20:55 +0900

    [인프런] 암호 - 문자열

---
+++ src/main/java/inflearn/string/암호/Main.java
@@ -0,0 +1,33 @@
+package inflearn.string.암호;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static StringBuilder sb = new StringBuilder();
+
+    public void solution(String s) {
+        s = s.replaceAll("#", String.valueOf(1)).replaceAll("\\*", String.valueOf(0));
+
+        int code = Integer.parseInt(s, 2);
+
+        sb.append((char) code);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Main main = new Main();
+        int n = Integer.parseInt(br.readLine());
+
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length() / 7; i++) {
+            String substring = str.substring(i * 7, (i * 7) + 7);
+            main.solution(substring);
+        }
+
+        System.out.println(sb);
+    }
+}
