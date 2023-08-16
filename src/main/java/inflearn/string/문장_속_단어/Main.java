bbc4652263e5bcbc680785cfa5989f97137bb97c
ohy1023 <zvyg1023@naver.com>
2023-08-16 12:20:32 +0900

    [인프런] 문장 속 단어 - 문자열

---
+++ src/main/java/inflearn/string/문장_속_단어/Main.java
@@ -0,0 +1,35 @@
+package inflearn.string.문장_속_단어;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static StringBuilder sb = new StringBuilder();
+
+    private static String solution(String st) {
+        String res = "";
+
+        int m = Integer.MIN_VALUE;
+
+        String[] s = st.split(" ");
+
+        for (String x : s) {
+            int len = x.length();
+            if (len > m) {
+                m = len;
+                res = x;
+            }
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String st = br.readLine();
+        System.out.println(solution(st));
+    }
+}
