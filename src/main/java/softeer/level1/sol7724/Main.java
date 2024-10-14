508aaa5c9b89894e7ccbe4be2d11cb74d1351b67
ohy1023 <zvyg1023@naver.com>
2024-10-14 16:24:51 +0900

    [소프티어] 진정한 효도 - 구현

---
+++ src/main/java/softeer/level1/sol7724/Main.java
@@ -0,0 +1,11 @@
+package softeer.level1.sol7724;
+
+/**
+ * 소프티어 진정한 효도 - 구현
+ * https://softeer.ai/practice/7724
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("엄마 아빠 사랑해요!");
+    }
+}
