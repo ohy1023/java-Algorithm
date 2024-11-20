322a596c9eb51a42cd2c77da7cc9d61a33625d2f
ohy1023 <vyg1023@gmail.com>
2024-11-20 21:50:46 +0900

    [프로그래머스] n^2 배열 자르기 - 구현

--- src/main/java/programmers/level2/sol87390/Solution.java
+++ src/main/java/programmers/level2/sol87390/Solution.java
@@ -1,24 +1,22 @@
 package programmers.level2.sol87390;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * 프로그래머스 Level.2 n^2 배열 자르기
+ * 프로그래머스 Level.2 n^2 배열 자르기 - 구현
  * https://school.programmers.co.kr/learn/courses/30/lessons/87390
  */
 class Solution {
-    public int[] solution(int n, long left, long right) {
-        int[] answer = {};
+    public List<Long> solution(int n, long left, long right) {
 
+        List<Long> arr = new ArrayList<>();
 
+        for (long i = left; i < right; i++) {
+            arr.add(Math.max(i / n, i % n) + 1);
+        }
 
-
-        return answer;
+        return arr;
     }
 
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(Arrays.toString(s.solution(3, 2, 5)));
-    }
 }
