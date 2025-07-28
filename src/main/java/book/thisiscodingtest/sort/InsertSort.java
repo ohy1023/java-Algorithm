0e10feabf24e9273663d17021ce1eb8f3eff739c
오형상 <vyg1023@gmail.com>
2025-07-28 17:49:01 +0900

    [이것이 코딩테스트다] 삽입 정렬

---
+++ src/main/java/book/thisiscodingtest/sort/InsertSort.java
@@ -0,0 +1,23 @@
+package book.thisiscodingtest.sort;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        Integer[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j >= 1; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
