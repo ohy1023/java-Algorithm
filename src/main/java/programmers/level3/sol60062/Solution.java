0caf77851d21e990651186c403437de4a3d93478
오형상 <vyg1023@gmail.com>
2025-08-18 10:43:39 +0900

    [프로그래머스] 외벽 점검 - 구현

--- src/main/java/programmers/level3/sol60062/Solution.java
+++ src/main/java/programmers/level3/sol60062/Solution.java
@@ -1,69 +1,58 @@
 package programmers.level3.sol60062;
 
-import java.util.*;
-
 /**
  * 프로그래머스 level 3 외벽 점검 - 구현
  * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/60062">...</a>
  */
 class Solution {
-    public int solution(int n, int[] weak, int[] dist) {
-        int W = weak.length;
-        int D = dist.length;
-        int answer = D + 1;
 
-        Integer[] extWeek = new Integer[W * 2];
-        
-        for (int i = 0; i < W; i++) {
-            extWeek[i] = weak[i];
-            extWeek[i + W] = weak[i] + n;
+    int N, MinCnt;
+    int[] Weak, Dist;
+
+    public void solve(int cnt, int pos, int visited) {
+        if (cnt > Dist.length) return;
+        if (cnt >= MinCnt) return;
+
+        for (int i = 0; i < Weak.length; i++) {
+            int nextPos = (pos + i) % Weak.length;
+
+            int diff = Weak[nextPos] - Weak[pos];
+
+            if (nextPos < pos) diff += N;
+
+            if (diff > Dist[Dist.length - cnt]) break;
+
+            visited |= 1 << nextPos;
         }
 
-        List<int[]> permutations = new ArrayList<>();
-        permute(dist, 0, permutations);
-
-        for (int start = 0; start < W; start++) {
-            for (int[] friends : permutations) {
-                int count = 1;
-                int limit = extWeek[start] + friends[count - 1];
-
-                for (int idx = start; idx < start + W; idx++) {
-                    if (extWeek[idx] > limit) {
-                        count++;
-                        if (count > D) break;
-                        limit = extWeek[idx] + friends[count - 1];
-                    }
-                }
-
-                answer = Math.min(count, answer);
-            }
-        }
-
-
-        return answer > D ? -1 : answer;
-    }
-
-    private void permute(int[] arr, int depth, List<int[]> result) {
-        if (arr.length == depth) {
-            result.add(arr.clone());
+        if (visited == (1 << Weak.length) - 1) {
+            MinCnt = cnt;
             return;
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            swap(arr, depth, i);
-            permute(arr, depth + 1, result);
-            swap(arr, depth, i);
+        for (int i = 0; i < Weak.length; i++) {
+            if ((visited & (1 << i)) != 0) continue;
+
+            solve(cnt + 1, i, visited);
         }
+
+
+
     }
 
-    private void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
-    }
+    public int solution(int n, int[] weak, int[] dist) {
 
-    public static void main(String[] args) {
-        Solution s = new Solution();
-        System.out.println(s.solution(12, new int[]{1, 5, 6, 10}, new int[]{1, 2, 3, 4,}));
+        N = n;
+        Weak = weak;
+        Dist = dist;
+        MinCnt = 9;
+
+        for (int i = 0; i < weak.length; i++) {
+            solve(1, i, 0);
+        }
+
+        if (MinCnt == 9) return -1;
+        return MinCnt;
+
     }
 }
